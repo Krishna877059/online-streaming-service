16c2fdf306763b67d3a455459d6d1811a4131efe
krishna <ks1150223@gmail.com>
2022-06-29 17:26:26 +0530

    Homescreen

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Container, Inner, Item,Pane, SubTitle,Image,Title } from './styles/jumbotron';
+
+export default function Jumbotron({children, direction = 'row',  ...restProps}) {
+  return(
+    <Item direction={direction}>
+    <Inner>
+        {children}
+    </Inner>
+        
+    </Item>
+  )
+}
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps}){
+     return <Container {...restProps}>{children}</Container>
+}
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps}){
+     return <Pane {...restProps}>{children}</Pane>
+}
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps}){
+      return <Title {...restProps}>{children}</Title>
+}
+
+Jumbotron.Image = function JumbotronImage({ children, ...restProps}){
+      return <Image {...restProps}/>;
+}
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps}){
+  return <SubTitle {...restProps}>{children}</SubTitle>
+}
